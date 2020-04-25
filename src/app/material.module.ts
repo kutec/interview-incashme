7ae6e62af329cae3d66c17c3a248cc237f9cfa4f
kutec <ku@kutec.co.in>
2020-04-25 10:29:27 +0530

    3 files added, 2 deleted

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
+@NgModule({
+  imports: [MatButtonToggleModule, MatTableModule, MatPaginatorModule],
+  exports: [MatButtonToggleModule, MatTableModule, MatPaginatorModule]
+})
+export class MaterialModule {}
